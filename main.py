fa37373655bed0690eca95fd50589300a1356fb1
erjbroek <broe0136@hz.nl>
2024-04-17 16:49:54 +0200

    rendering + moving pipes on window

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 import config
+import components
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -11,13 +12,30 @@ def quit_game():
       pygame.quit()
       exit()
 
+def generate_pipe():
+  config.pipes.append(components.Pipes(config.win_width))
+
 def main():
+  pipes_spawn_delay = 10
+
   while True:
     quit_game()
 
     config.window.fill((0, 0, 0))
-
     config.ground.render(config.window)
+
+    if pipes_spawn_delay <= 0:
+      generate_pipe()
+      pipes_spawn_delay = 200
+    pipes_spawn_delay -= 1
+
+
+    for pipe in config.pipes:
+      pipe.render(config.window)
+      pipe.update()
+      if pipe.off_screen:
+        config.pipes.remove(pipe)
+
 
     clock.tick(60)
     pygame.display.flip()
