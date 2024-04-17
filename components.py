fe371eaf7f81345eb1da80a11cc2dd048843e823
erjbroek <broe0136@hz.nl>
2024-04-17 16:42:03 +0200

    setup pipes class with all atributes

--- components.py
+++ components.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 class Ground:
@@ -10,3 +11,33 @@ class Ground:
 
   def render(self, window):
     pygame.draw.rect(window, (255, 255, 255), self.rect)
+
+class Pipes:
+  width = 15
+  opening_height = 100
+  color = (0, 255, 255)
+
+  def __init__(self, win_width):
+    self.x = win_width
+    self.bottom_height = random.randint(10, 300)
+    self.top_height = Ground.ground_posY - self.bottom_height - self.opening_height
+    self.bottom_rect, self.top_rect = pygame.Rect(0, 0, 0, 0), pygame.Rect(0, 0, 0, 0)
+    self.passsed = False
+    self.off_screen = False
+
+  def render(self, window):
+    self.bottom_rect = pygame.Rect(self.x, Ground.ground_posY - self.bottom_height, self.width, self.bottom_height)
+    pygame.draw.rect(window, self.color, self.bottom_rect)
+
+    self.top_rect = pygame.Rect(self.x, 0, self.width, self.top_height)
+    pygame.draw.rect(window, self.color, self.top_rect)
+
+  def update(self):
+    self.x -= 1
+    if self.x + Pipes.width <= 50:
+      self.passed = True
+      self.color = (255, 0, 255)
+
+    if self.x + self.width <= 0:
+      self.off_screen = True
+
--- config.py
+++ config.py
@@ -6,3 +6,4 @@ win_width = 550
 window = pygame.display.set_mode((win_width, win_height))
 
 ground = components.Ground(win_width)
+pipes = []
